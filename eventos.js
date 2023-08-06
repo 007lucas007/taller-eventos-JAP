9c5e72e8b718b56aaedcbcdadf88e39059d685fe
007lucas007 <lumionar@hotmail.com>
2023-08-06 19:04:54 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -1,5 +1,12 @@
 const div = document.querySelector(".div-button")
+const boton = document.querySelector(".button-saludar")
 
-div.addEventListener('click', function() {
+div.addEventListener('click', function(e) {
+    if(e.target === div){
     alert('Hola! Soy el div')
+ }
 });
+
+boton.addEventListener('click', function(){
+    alert('Hola!');
+})
